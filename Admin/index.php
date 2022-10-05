9de11c18c08775bed357ac7717dd82340aba274e
Duta <84756800+Duta46@users.noreply.github.com>
2022-10-06 05:26:28 +0700

    Update index.php
    Menambahkan link logout

--- Admin/index.php
+++ Admin/index.php
@@ -9,6 +9,9 @@
   </head>
   <body>
     
+    <h1>Halaman Admin</h1>
+    <a href="logout.php">Logout</a>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
